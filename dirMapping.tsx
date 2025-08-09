1622335f47b5a10f955b465fa43b8017a8139e19
masterdev090195 <hackzdecoder@gmail.com>
2025-08-10 01:09:33 +0800

    Typescript Node Backend

--- dirMapping.tsx
+++ dirMapping.tsx
@@ -7,8 +7,8 @@ const __dirname = path.dirname(__filename);
 const baseDir = path.resolve(__dirname, ".");
 
 export const paths = {
-    routes: pathToFileURL(path.join(baseDir, "routes")).href,       // <-- outside src
     controllers: pathToFileURL(path.join(baseDir, "src/controllers")).href,
     models: pathToFileURL(path.join(baseDir, "src/models")).href,
+    routes: pathToFileURL(path.join(baseDir, "routes")).href,
     config: pathToFileURL(path.join(baseDir, "config")).href,
 };
